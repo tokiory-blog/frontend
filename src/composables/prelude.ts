4e0ec403d8760127420fe1083f2e939ddcd7b25c
kiotosi <shilo@firecode.ru>
2023-03-31 18:15:59 +0300

    fix(layout): fixed layout composition

---
+++ src/composables/prelude.ts
@@ -0,0 +1,17 @@
+// Styles
+import '../styles/-colors.scss';
+import '../styles/-breakpoints.scss';
+import '../styles/-size.scss';
+import '../styles/+common.scss';
+import 'normalize.css';
+
+// Jetbrains font
+import '@fontsource/jetbrains-mono/400.css';
+import '@fontsource/jetbrains-mono/400-italic.css';
+import '@fontsource/jetbrains-mono/500.css';
+import '@fontsource/jetbrains-mono/500-italic.css';
+
+// Manrope font
+import '@fontsource/manrope/300.css';
+import '@fontsource/manrope/400.css';
+import '@fontsource/manrope/500.css';
