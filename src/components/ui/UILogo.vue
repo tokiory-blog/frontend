c51549335f8d035880c1221dc0ff24ae8c7e456d
kiotosi <shilo@firecode.ru>
2023-03-31 19:06:33 +0300

    feat(header): add header for project

--- src/components/ui/UILogo.vue
+++ src/components/ui/UILogo.vue
@@ -1,16 +1,20 @@
 <template>
-  <pre class="logo">
-      . . o x o x o x o . . .
-    o      _______
-    .][__n_n_ |DD[  
-    >(________|__|
-    _/oo OOOOO oo\`
-    -+-+-+-+-+-+-+-+
-  </pre>
+  <a class="logo" href="/">
+    <pre class="logo__image">
+        . . o x o x o x o . . .
+      o      _______
+      .][__n_n_ |DD[  
+      >(________|__|
+      _/oo OOOOO oo\`
+      -+-+-+-+-+-+-+-+
+    </pre>
+  </a>
 </template>
 
 <style lang="scss" scoped>
 .logo {
   font-size: 6px;
+  text-decoration: none;
+  color: inherit;
 }
 </style>
