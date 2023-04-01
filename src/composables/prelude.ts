4c3b87a6f7fb62957e23f6c52db834104da6d18f
Daniil Shilo <crackidocky@gmail.com>
2023-04-01 22:04:06 +0300

    feat(fonts): add static fonts (#4)
    Co-authored-by: kiotosi <shilo@firecode.ru>

--- src/composables/prelude.ts
+++ src/composables/prelude.ts
@@ -3,15 +3,5 @@ import '@/styles/-colors.scss';
 import '@/styles/-breakpoints.scss';
 import '@/styles/-size.scss';
 import '@/styles/+common.scss';
+import '@/styles/+fonts.scss';
 import 'normalize.css';
-
-// Jetbrains font
-import '@fontsource/jetbrains-mono/400.css';
-import '@fontsource/jetbrains-mono/400-italic.css';
-import '@fontsource/jetbrains-mono/500.css';
-import '@fontsource/jetbrains-mono/500-italic.css';
-
-// Manrope font
-import '@fontsource/manrope/300.css';
-import '@fontsource/manrope/400.css';
-import '@fontsource/manrope/500.css';
