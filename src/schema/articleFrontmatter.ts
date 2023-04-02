53131708ee4dddfa9d24c0ffb18dedda0cf15345
kiotosi <shilo@firecode.ru>
2023-04-02 10:56:57 +0300

    feat(content): add dates to the content

--- src/schema/articleFrontmatter.ts
+++ src/schema/articleFrontmatter.ts
@@ -3,5 +3,6 @@ import { z } from "astro:content";
 export default  z.object({
   title: z.string(),
   tags: z.array(z.string()),
-  description: z.string()
+  description: z.string(),
+  publicationDate: z.string()
 });
