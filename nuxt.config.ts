f2ea6c892cb4f60e06c6cd320b3237d37b94cdca
kiotosi <shilo@firecode.ru>
2023-04-05 23:01:05 +0300

    fix(nuxt.config.ts): fixed nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,9 @@
 export default defineNuxtConfig({
-  app: {
-    head: {
-      charset: 'UTF-8',
-      viewport: 'width=device-width, initial-scale=1',
-    }
-  },
   modules: [
     '@nuxt/content'
   ],
   content: {
     highlight: {
-      // Theme used in all color schemes.
       theme: 'dark-plus'
     }
   }
