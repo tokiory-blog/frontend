32b0e3a61c615e6ba558cc552f23cc9caf932769
kiotosi <shilo@firecode.ru>
2023-04-06 15:06:10 +0300

    fix(nuxt): add more paths, and remove standard paths

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
+import {fileURLToPath} from 'url';
 export default defineNuxtConfig({
   modules: [
     '@nuxt/content'
   ],
+  alias: {
+    '@': fileURLToPath(new URL('./', import.meta.url)),
+    '@d': fileURLToPath(new URL('./data', import.meta.url)),
+    '@cmp': fileURLToPath(new URL('./components', import.meta.url)),
+    '@cms': fileURLToPath(new URL('./components', import.meta.url)),
+    '@p': fileURLToPath(new URL('./pages', import.meta.url)),
+    'public': fileURLToPath(new URL('./public', import.meta.url)),
+  },
   content: {
     highlight: {
       theme: 'dark-plus'
