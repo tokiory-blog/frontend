57084fac43c48802d6652507ece8a4f6918dc325
tokiory <crackidocky@gmail.com>
2023-04-17 13:02:05 +0300

    feat(nickname): update site name

--- components/base/BaseHero.vue
+++ components/base/BaseHero.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="hero">
     <div class="hero__text">
-      Станция Окио.
+      Станция Токиори.
     </div>
     <HeroAscii class="hero__ascii" />
     <div class="hero__description">
