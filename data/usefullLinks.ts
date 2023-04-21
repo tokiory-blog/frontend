bf557b011f5ce853ffe333491ffd777a88b3d22c
tokiory <crackidocky@gmail.com>
2023-04-21 12:47:20 +0300

    feat(link): add more links

--- data/usefullLinks.ts
+++ data/usefullLinks.ts
@@ -1,3 +1,5 @@
+import { Link } from "@unhead/schema";
+
 const designSection: LinkSection = {
   name: "Дизайн",
   child: [
@@ -77,6 +79,18 @@ const documentationSection: LinkSection = {
   ]
 };
 
+const architectureSection: LinkSection = {
+  name: "Архитектура",
+  child: [
+    {
+      name: "Pattern Guru",
+      url: "https://refactoring.guru/ru/design-patterns",
+      description: "Сайт для просмотра всевозможных паттернов для архитектуры. Отличается краткостью, удобной навигацией и " +
+        "легкой подачей материала"
+    }
+  ],
+};
+
 const databaseSection: LinkSection = {
   name: "Базы данных",
   child: [
@@ -117,13 +131,59 @@ const extraSection: LinkSection = {
     }
   ]
 };
+
+const articles = {
+  go: [
+    {
+      name: "[go.dev] Strings, bytes, runes and characters in Go - The Go Programming Language",
+      url: "https://go.dev/blog/strings",
+      description: "Описывается работа строк, рун и символов в Go"
+    },
+  ],
+  rust: [
+    {
+      name: "[Proglib] Владение и заимствование в Rust: детально о Lifetime для начинающих и более опытных",
+      url: "https://proglib.io/p/vladenie-i-zaimstvovanie-v-rust-detalno-o-lifetime-dlya-nachinayushchih-i-bolee-opytnyh-2021-10-22?ysclid=lgqa88ln16253142064",
+      description: "Коротко и понятно описывается работа borrow checker'а",
+    },
+    {
+      name: "[Habr] Traits из коробки",
+      url: "https://habr.com/ru/articles/277461/",
+      description: "Описываются трейты, которые предложены стандартной библиотекой Rust"
+    }
+  ],
+  extra: [
+    {
+      name: "[Habr] Шаблоны проектирования с человеческим лицом",
+      url: "https://habr.com/ru/companies/vk/articles/325492/",
+      description: "Легко и просто описываются паттерны проектирования"
+    }
+  ]
+};
+
+const articleSection: LinkSection = {
+  name: "Статьи заслуживающие внимания",
+  child: Object.keys(articles).reduce<LinkSectionItem[]>((acc, key): LinkSectionItem[] => {
+    const articleTag = key[0].toUpperCase() + key.slice(1);
+    const articleList = articles[key as keyof typeof articles].map(article => {
+      return {
+        ...article,
+        name: `${articleTag}: ${article.name}`,
+      };
+    });
+    
+    return [...acc, ...articleList];
+  }, [])
+};
 export default [
   designSection,
   readmeSection,
   documentationSection,
+  architectureSection,
   databaseSection,
   deploymentSection,
   extraSection,
+  articleSection,
 ];
 
 export interface LinkSection {
