cdff49305288447d03b44c0512a6169f0e9a8abd
Даниил Шило <tokiory.personal@gmail.com>
2023-05-25 08:13:46 +0300

    feat(links): add links for customization

--- data/usefullLinks.ts
+++ data/usefullLinks.ts
@@ -1,5 +1,3 @@
-import { Link } from "@unhead/schema";
-
 const designSection: LinkSection = {
   name: "Дизайн",
   child: [
@@ -142,6 +140,58 @@ const extraSection: LinkSection = {
   ]
 };
 
+const customFonts: LinkSection = {
+  name: "Кастомизация: Шрифты",
+  child: [
+    {
+      name: "Sudo",
+      url: "https://www.kutilek.de/sudo-font/",
+    },
+    {
+      name: "Mononoki",
+      url: "http://madmalik.github.io/mononoki/",
+    },
+    {
+      name: "Iosevka",
+      url: "https://typeof.net/Iosevka/",
+    },
+    {
+      name: "Input",
+      url: "https://input.djr.com/preview/?size=14&language=python&theme=solarized-dark&family=InputSans&width=300&weight=300&line-height=1.2&a=0&g=0&i=0&l=0&zero=0&asterisk=0&braces=0&preset=default&customize=please",
+    },
+    {
+      name: "iA Writer Mono",
+      url: "https://ia.net/topics/a-typographic-christmas",
+    },
+    {
+      name: "Cartograph",
+      url: "https://connary.com/cartograph.html",
+    },
+    {
+      name: "Fantasque Sans Mono",
+      url: "https://github.com/belluzj/fantasque-sans",
+    }
+  ]
+};
+
+const customThemes: LinkSection = {
+  name: "Кастомизация: Темы",
+  child: [
+    {
+      name: "Tomorrow",
+      url: "https://github.com/chriskempson/tomorrow-theme",
+    },
+    {
+      name: "Catppuccin",
+      url: "https://github.com/catppuccin/catppuccin"
+    },
+    {
+      name: "Solarized",
+      url: "https://ethanschoonover.com/solarized/",
+    },
+  ],
+};
+
 const articles = {
   go: [
     {
@@ -193,6 +243,8 @@ export default [
   databaseSection,
   deploymentSection,
   extraSection,
+  customFonts,
+  customThemes,
   articleSection,
 ];
 
