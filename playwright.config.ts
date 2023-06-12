2e178aacd4f874fec35edca188585970f76d2749
tokiory <tokiory.personal@gmail.com>
2023-06-12 17:36:30 +0300

    fix(playwright): fixed command line arguments for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,13 +2,35 @@ import { defineConfig, devices } from "@playwright/test";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+const localBrowsers = [
+  {
+    name: "chromium",
+    use: { ...devices["Desktop Chrome"] },
+  },
+  {
+    name: "firefox",
+    use: { ...devices["Desktop Firefox"] },
+  },
+  {
+    name: "webkit",
+    use: { ...devices["Desktop Safari"] },
+  },
+];
+
+const ciBrowsers = [
+  {
+    name: "chromium",
+    use: { ...devices["Desktop Chrome"] },
+  },
+];
+
 // @see https://playwright.dev/docs/test-configuration
 export default defineConfig({
   testDir: "./tests/scenario",
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: 3,
+  workers: process.env.CI ? 1 : 2,
   reporter: process.env.CI ? "dot" : "list",
   use: {
     baseURL: `http://127.0.0.1:${process.env.PORT}`,
@@ -17,22 +39,7 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-  ],
+  projects: process.env.CI ? ciBrowsers : localBrowsers,
 
   /* Run your local dev server before starting the tests */
   webServer: {
